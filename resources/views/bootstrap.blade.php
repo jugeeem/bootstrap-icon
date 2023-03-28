0a4c3c79e4242bdcd7430650f61597f3afaf5464
jugemu373 <1004traitor@gmail.com>
2023-03-28 23:40:46 +0900

    second

---
+++ resources/views/bootstrap.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Laravel Sail with Bootstrap</title>
+    @vite(['resources/css/app.css','resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+
+<body>
+    <div class="container">
+        <i class="bi bi-{icon-name}"></i>
+        <i class="bi bi-heart"></i>
+        <h1>Laravel Sail with Bootstrap</h1>
+        <p>Welcome to our Laravel Sail application with Bootstrap!</p>
+    </div>
+</body>
+
+</html>
